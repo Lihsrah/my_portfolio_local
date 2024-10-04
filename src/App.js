6e48f49088a2eda18b1afe30c866b8b3d392e100
Ankit Lakhlan <ankitlakhlan03@gmail.com>
2024-10-04 20:40:17 +0530

    projects page first section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
-import Home from './assets/PAGES/Home';
-import Footer from './components/Footer';
+import Home from "./assets/PAGES/Home";
+import Footer from "./components/Footer";
+import Projects from "./assets/PAGES/Projects";
 
 const App = () => {
   return (
@@ -10,10 +11,11 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
